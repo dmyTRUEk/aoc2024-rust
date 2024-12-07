d0dc9712d8930a932a0ba29256ffe1262e27cbed
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2024-12-07 11:43:15 +0200

    optimize day6 part2 by ~1000x

--- src/bin/day6_part2.rs
+++ src/bin/day6_part2.rs
@@ -1,6 +1,13 @@
 //! Advent of Code - Day 6 Part 2 solution.
 
-use std::{fs, usize};
+#![deny(
+	unused_assignments,
+	unused_results,
+)]
+
+use std::{collections::HashSet, fs};
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 
 fn main() {
@@ -11,44 +18,51 @@ fn main() {
 fn solve(filepath: &str) -> usize {
 	let text = fs::read_to_string(filepath).unwrap();
 	let m = text_to_vec2d(text);
-	// println!("{}", map_to_string(&m));
 	let guard_pos = get_guard_state(&m).unwrap().to_pos();
-	let trace_positions = get_trace_positions(m.clone());
-	trace_positions.into_iter()
+	let trace_positions = get_trace_positions(m.clone(), guard_pos);
+	trace_positions
+		// .into_iter()
+		.into_par_iter()
 		.filter(|p| *p != guard_pos)
 		.filter(|p| {
 			let mut m = m.clone();
 			m[p.y as usize][p.x as usize] = '#';
-			is_looped(m)
+			is_looped(m, guard_pos)
 		})
 		.count()
 }
 
 type Map = Vec<Vec<char>>;
 
-fn is_looped(mut m: Map) -> bool {
-	let mut states: Vec<GuardState> = vec![];
+fn is_looped(mut m: Map, mut guard_pos: Pos) -> bool {
+	let mut states: HashSet<GuardState> = HashSet::new();
 	loop {
-		let Some(guard_state) = get_guard_state(&m) else { return false };
+		let Some(dir) = get_guard_dir(&m, guard_pos) else { return false };
+		let guard_state = GuardState { x: guard_pos.x, y: guard_pos.y, dir };
 		if states.contains(&guard_state) { return true }
-		states.push(guard_state);
-		step(&mut m);
+		let _ = states.insert(guard_state);
+		guard_pos = step(&mut m, guard_pos);
 	}
 }
 
-fn get_trace_positions(mut m: Map) -> Vec<Pos> {
-	assert!(!is_looped(m.clone()));
-	let mut positions: Vec<Pos> = vec![];
+fn get_trace_positions(mut m: Map, mut guard_pos: Pos) -> Vec<Pos> {
+	// assert!(!is_looped(m.clone()));
+	let mut positions = HashSet::<Pos>::new();
 	loop {
-		let Some(guard_state) = get_guard_state(&m) else { return positions };
+		let Some(guard_state) = get_guard_state_optim(&m, guard_pos) else {
+			return positions.into_iter().collect()
+		};
 		let p = guard_state.to_pos();
-		if !positions.contains(&p) { positions.push(p) };
-		step(&mut m);
+		if !positions.contains(&p) {
+			let _ = positions.insert(p);
+		}
+		guard_pos = step(&mut m, p);
 	}
 }
 
-fn step(m: &mut Map) {
-	let GuardState{x, y, dir} = get_guard_state(m).unwrap();
+/// returns new guard position
+fn step(m: &mut Map, pos@Pos{x, y}: Pos) -> Pos {
+	let dir = get_guard_dir(m, pos).unwrap();
 	let next_x = x + match dir {
 		'<' => -1,
 		'>' => 1,
@@ -63,13 +77,16 @@ fn step(m: &mut Map) {
 	};
 	if is_blocked_at(m, Pos{x: next_x, y: next_y}) {
 		m[y as usize][x as usize] = next_dir(dir);
+		pos
 	} else {
 		m[y as usize][x as usize] = '.';
 		// m[next_y][next_x] = dir;
-		if next_x < 0 || next_y < 0 { return }
-		if let Some(new_pos) = m.get_mut(next_y as usize).map(|l| l.get_mut(next_x as usize)).flatten() {
-			*new_pos = dir;
+		if !(next_x < 0 || next_y < 0) {
+			if let Some(new_pos) = m.get_mut(next_y as usize).map(|l| l.get_mut(next_x as usize)).flatten() {
+				*new_pos = dir;
+			}
 		}
+		Pos { x: next_x, y: next_y }
 	}
 }
 
@@ -79,11 +96,11 @@ fn next_dir(dir: char) -> char {
 	DIRS[(DIRS.iter().position(|&d| d == dir).unwrap() + 1) % 4]
 }
 
-fn is_dir(c: &&char) -> bool {
+fn is_dir(c: &char) -> bool {
 	DIRS.contains(c)
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Pos { x: i32, y: i32 }
 
 fn is_blocked_at(m: &Map, Pos{x, y}: Pos) -> bool {
@@ -94,11 +111,7 @@ fn is_blocked_at(m: &Map, Pos{x, y}: Pos) -> bool {
 		.is_some_and(|&c| c == '#')
 }
 
-// fn get_guard_xy(map: &Map) -> (usize, usize) {
-// 	todo!()
-// }
-
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct GuardState { x: i32, y: i32, dir: char }
 impl GuardState {
 	fn to_pos(&self) -> Pos {
@@ -106,7 +119,21 @@ impl GuardState {
 	}
 }
 
+fn get_guard_state_optim(m: &Map, Pos{x, y}: Pos) -> Option<GuardState> {
+	if x < 0 || y < 0 { return None }
+	let Some(&dir) = m.get(y as usize).map(|l| l.get(x as usize)).flatten() else { return None };
+	assert!(is_dir(&dir));
+	Some(GuardState { x, y, dir })
+}
+
+fn get_guard_dir(m: &Map, Pos{x, y}: Pos) -> Option<char> {
+	if x < 0 || y < 0 { return None }
+	let Some(&dir) = m.get(y as usize).map(|l| l.get(x as usize)).flatten() else { return None };
+	Some(dir)
+}
+
 /// returns X, Y, Direction
+#[deprecated = "if possible, use optimized version: `get_guard_dir` or `get_guard_state_optim`"]
 fn get_guard_state(m: &Map) -> Option<GuardState> {
 	m.iter().enumerate()
 		.filter_map(|(y, l)| l.iter().enumerate()
